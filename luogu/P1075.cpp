ba113f7c36d3bd6f5a36e6d6308c33578d613160
Crabtime <68049817+Crabtime@users.noreply.github.com>
2020-08-02 20:41:00 +0800

    Add files via upload

---
+++ luogu/P1075.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    std:: cin >> n;
+    for(int i=2;i<n;i++)
+    {
+        if (n%i==0)
+        {
+            std:: cout << n/i;
+            break;
+        }
+    }
+    return 0;
+}
